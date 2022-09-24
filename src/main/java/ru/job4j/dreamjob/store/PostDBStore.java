c69693f0b537093224fda7f7779b313e98ad9190
AMEMELYANOV <emelyanov.am@gmail.com>
2022-09-24 12:04:36 +0300

    1. Подключение к базе в веб приложении. Хранение вакансий. [#504859 #346009]

--- src/main/java/ru/job4j/dreamjob/store/PostDBStore.java
+++ src/main/java/ru/job4j/dreamjob/store/PostDBStore.java
@@ -1,20 +1,26 @@
 package ru.job4j.dreamjob.store;
 
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 import ru.job4j.dreamjob.model.City;
 import ru.job4j.dreamjob.model.Post;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Timestamp;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public class PostDBStore {
+    private static final Logger LOG = LoggerFactory.getLogger(PostDBStore.class.getName());
+    private static final String FIND_ALL_SELECT = "SELECT * FROM post ORDER BY id";
+    private static final String FIND_BY_ID_SELECT = "SELECT * FROM post WHERE id = ?";
+    private static final String INSERT_INTO = "INSERT INTO post(name, description, created, "
+            + "visible, city_id) VALUES (?, ?, ?, ?, ?)";
+    private static final String UPDATE = "UPDATE post SET name = ?, description = ?, visible = ?, "
+            + "city_id = ? WHERE id = ?";
 
     private final BasicDataSource pool;
 
@@ -25,27 +31,22 @@ public class PostDBStore {
     public List<Post> findAll() {
         List<Post> posts = new ArrayList<>();
         try (Connection cn = pool.getConnection();
-             PreparedStatement ps = cn.prepareStatement("SELECT * FROM post ORDER BY id")
+             PreparedStatement ps = cn.prepareStatement(FIND_ALL_SELECT)
         ) {
             try (ResultSet it = ps.executeQuery()) {
                 while (it.next()) {
-                    posts.add(new Post(it.getInt("id"), it.getString("name"),
-                            it.getString("description"), it.getBoolean("visible"),
-                            it.getTimestamp("created").toLocalDateTime(),
-                            new City(it.getInt("city_id"), null)));
+                    posts.add(getPostFromResultSet(it));
                 }
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            LOG.error("Исключение в методе findAllPosts() класс PostDBStore", e);
         }
         return posts;
     }
 
     public Post add(Post post) {
         try (Connection cn = pool.getConnection();
-             PreparedStatement ps = cn.prepareStatement("INSERT INTO post(name, description, "
-                             + " created, visible, city_id) "
-                             + "VALUES (?, ?, ?, ?, ?)",
+             PreparedStatement ps = cn.prepareStatement(INSERT_INTO,
                      PreparedStatement.RETURN_GENERATED_KEYS)
         ) {
             ps.setString(1, post.getName());
@@ -60,16 +61,14 @@ public class PostDBStore {
                 }
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            LOG.error("Исключение в методе add() класс PostDBStore", e);
         }
         return post;
     }
 
     public void update(Post post) {
         try (Connection cn = pool.getConnection();
-             PreparedStatement ps = cn.prepareStatement(
-                     "UPDATE post SET name = ?, description = ?, visible = ?, city_id = ? "
-                             + "WHERE id = ?")
+             PreparedStatement ps = cn.prepareStatement(UPDATE)
         ) {
             ps.setString(1, post.getName());
             ps.setString(2, post.getDescription());
@@ -78,26 +77,30 @@ public class PostDBStore {
             ps.setInt(5, post.getId());
             ps.execute();
         } catch (Exception e) {
-            e.printStackTrace();
+            LOG.error("Исключение в методе update() класс PostDBStore", e);
         }
     }
 
     public Optional<Post> findById(int id) {
         try (Connection cn = pool.getConnection();
-             PreparedStatement ps = cn.prepareStatement("SELECT * FROM post WHERE id = ?")
+             PreparedStatement ps = cn.prepareStatement(FIND_BY_ID_SELECT)
         ) {
             ps.setInt(1, id);
             try (ResultSet it = ps.executeQuery()) {
                 if (it.next()) {
-                    return Optional.of(new Post(it.getInt("id"), it.getString("name"),
-                            it.getString("description"), it.getBoolean("visible"),
-                            it.getTimestamp("created").toLocalDateTime(),
-                            new City(it.getInt("city_id"), null)));
+                    return Optional.of(getPostFromResultSet(it));
                 }
             }
         } catch (Exception e) {
-            e.printStackTrace();
+            LOG.error("Исключение в методе findById() класс PostDBStore", e);
         }
         return Optional.empty();
+    }
+
+    private static Post getPostFromResultSet(ResultSet it) throws SQLException {
+        return new Post(it.getInt("id"), it.getString("name"),
+                it.getString("description"), it.getBoolean("visible"),
+                it.getTimestamp("created").toLocalDateTime(),
+                new City(it.getInt("city_id"), null));
     }
 }
