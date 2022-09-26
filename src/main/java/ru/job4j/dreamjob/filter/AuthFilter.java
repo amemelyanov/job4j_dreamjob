180468faeedb582195a29f01bc1e74037d843913
AMEMELYANOV <emelyanov.am@gmail.com>
2022-09-26 20:11:59 +0300

    3. Filter [#504865 #346407]

--- src/main/java/ru/job4j/dreamjob/filter/AuthFilter.java
+++ src/main/java/ru/job4j/dreamjob/filter/AuthFilter.java
@@ -5,9 +5,14 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 @Component
 public class AuthFilter implements Filter {
+    private final Set<String> mappings = new HashSet<>(
+            Arrays.asList("loginPage", "login", "registrationPage", "registration"));
 
     @Override
     public void doFilter(
@@ -17,8 +22,7 @@ public class AuthFilter implements Filter {
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse res = (HttpServletResponse) response;
         String uri = req.getRequestURI();
-        if (uri.endsWith("loginPage") || uri.endsWith("login")
-                || uri.endsWith("registrationPage") || uri.endsWith("registration")) {
+        if (isAllowed(uri)) {
             chain.doFilter(req, res);
             return;
         }
@@ -27,5 +31,9 @@ public class AuthFilter implements Filter {
             return;
         }
         chain.doFilter(req, res);
+    }
+
+    private boolean isAllowed(String uri) {
+        return mappings.stream().anyMatch(uri::endsWith);
     }
 }
